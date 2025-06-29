5a2527ef54d8060176c42666ff77e93abb17cf6d
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2025-06-29 20:36:03 +0300

    more changes

--- source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/CaesarCipher.java
+++ source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/CaesarCipher.java
@@ -1,4 +1,55 @@
 package com.javarush.module1.javasyntax.ryaroshenko.project1;
 
+/**
+ * Клас шифрування/дешифрування потока символів алгоритмом Цезаря
+ */
 public class CaesarCipher {
+    // Додаткові символи до алфавіту шифрування
+    private char[] symbols = new char[]{'.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+    // Кількість літер англійського алфавіту в нижньому регістрі
+    private int count_of_lower_symbols = ('z' - 'a') + 1;
+    // Кількість літер англійського алфавіту в верхньому регістрі
+    private int count_of_upper_symbols = ('Z' - 'A') + 1;
+    // Алфавіт шифрування
+    private char[] alphabet = new char[count_of_lower_symbols + count_of_upper_symbols + symbols.length];
+
+    /**
+     * Constructor
+     */
+    CaesarCipher() {
+        fillAlphabet();
+    }
+
+    /**
+     * Ініціалізація алфавіту
+     */
+    private void fillAlphabet() {
+        int i = 0;
+
+        for (char symbol = 'a'; symbol <= 'z'; symbol++) {
+            alphabet[i] = symbol;
+            i++;
+        }
+
+        for (char symbol = 'A'; symbol <= 'Z'; symbol++) {
+            alphabet[i] = symbol;
+            i++;
+        }
+
+        for (int index = 0; index < symbols.length; index++) {
+            alphabet[i] = symbols[index];
+            i++;
+        }
+    }
+
+    /**
+     * Реалізація методу toString()
+     *
+     * @return String - увесь алфавіт у вигляді рядка
+     */
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(alphabet);
+        return result.toString();
+    }
 }
--- source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileCryptor.java
+++ source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileCryptor.java
@@ -1,7 +1,11 @@
 package com.javarush.module1.javasyntax.ryaroshenko.project1;
 
+/**
+ * Основний клас шифратора/дешифратора файлів
+ */
 public class FileCryptor {
     public static void main(String[] args) {
-        System.out.println("Hello1");
+        CaesarCipher cipher = new CaesarCipher();
+        System.out.println(cipher);
     }
 }
