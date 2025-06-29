4aaa3c37a6ba850f43561574a39fae8905be9dfd
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2025-06-30 00:11:21 +0300

    [+] added class FileService

--- source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileCryptor.java
+++ source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileCryptor.java
@@ -1,5 +1,7 @@
 package com.javarush.module1.javasyntax.ryaroshenko.project1;
 
+import java.util.List;
+
 /**
  * Основний клас шифратора/дешифратора файлів
  */
@@ -7,5 +9,9 @@ public class FileCryptor {
     public static void main(String[] args) {
         CaesarCipher cipher = new CaesarCipher();
         System.out.println(cipher);
+
+        FileService file = new FileService();
+        List<Character> list = file.readFile(".gitignore");
+        System.out.println(list.size());
     }
 }
