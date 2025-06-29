046e5ebaac55f8fb2080ada859b1013cd6d277b0
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2025-06-29 18:22:07 +0300

    [+] created project package

---
+++ source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileCryptor.java
@@ -0,0 +1,7 @@
+package com.javarush.module1.javasyntax.ryaroshenko.project1;
+
+public class FileCryptor {
+    public static void main(String[] args) {
+        System.out.println("Hello1");
+    }
+}
