d76f538b112d401341617b1fa437e595f85aab93
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2025-06-30 16:32:35 +0300

    added method FileService.writeFile

--- source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileCryptor.java
+++ source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileCryptor.java
@@ -1,17 +1,22 @@
 package com.javarush.module1.javasyntax.ryaroshenko.project1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 /**
  * Основний клас шифратора/дешифратора файлів
  */
 public class FileCryptor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CaesarCipher cipher = new CaesarCipher();
         System.out.println(cipher);
 
         FileService file = new FileService();
         List<Character> list = file.readFile(".gitignore");
         System.out.println(list.size());
+        Files.deleteIfExists(Path.of( ".gitignore_copy"));
+        file.writeFile(".gitignore_copy", list);
     }
 }
--- source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileService.java
+++ source/src/com/javarush/module1/javasyntax/ryaroshenko/project1/FileService.java
@@ -1,12 +1,19 @@
 package com.javarush.module1.javasyntax.ryaroshenko.project1;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Клас для роботи з файлами
+ */
 public class FileService {
+    /**
+     * readFile - завантаження змісту файлу в List<Character>
+     *
+     * @param filePath - назва файлу разом з його шляхом
+     * @return List<Character> - колекція символів з файлу
+     */
     public List<Character> readFile(String filePath) {
         List<Character> list = new ArrayList<>();
 
@@ -23,5 +30,26 @@ public class FileService {
         }
 
         return list;
+    }
+
+    /**
+     * writeFile - збереження змісту List<Character>
+     *
+     * @param filePath - назва файлу разом з його шляхом
+     * @param list     - колекція символів для файлу
+     */
+    public void writeFile(String filePath, List<Character> list) {
+        if (list == null)
+            return;
+
+        try {
+            BufferedWriter buffer = new BufferedWriter(new FileWriter(filePath));
+
+            for (Character value : list)
+                buffer.write(value.charValue());
+
+            buffer.close();
+        } catch (IOException e) {
+        }
     }
 }
